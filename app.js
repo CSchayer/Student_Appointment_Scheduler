a4a50f0c2060e9875c34b0c20d111eab4d58e98c
jsing25 <james.singleton133@gmail.com>
2016-03-31 18:14:49 -0500

    Added directories for public and views. Created the index file for the home page.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // MAIN
 
+var express = require('express');
+var app = express();
+var db = require('./database');
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+// GET Home Page
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port);
 
