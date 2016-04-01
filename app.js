2f0d29d3331a6545f7eafc0f376ea5865b510db7
jsing25 <james.singleton133@gmail.com>
2016-03-31 20:25:53 -0500

    Created the advisor route and moved calendar files to public folder

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 
 // GET Home Page
 app.get('/', function(req, res) {
     res.render('index');
+});
+
+//GET Advisor Page
+app.get('/advisor', function(req, res) {
+    res.render('advisor.html');
 });
 
 app.listen(port);
