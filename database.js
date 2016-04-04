c296a91021333bbd3678644d865a167e76d0a5c3
jsing25 <james.singleton133@gmail.com>
2016-04-03 22:45:54 -0500

    Added GET and POST routes to add items to the database

--- database.js
+++ database.js
@@ -1,4 +1,8 @@
 // Database connection
+// Not currently using this file in the app.
+// It can be required in the main app.js
+// by adding:
+// var db = require('./database')
 
 var mongoose = require('mongoose');
 var config = require('./config');
