fcfad478d1b6a2af65c800b869629fb760accf26
chrisschayer <cschay1@gmail.com>
2016-04-16 00:40:34 -0500

    Added dynamic counselor selector population.
    Can obtain time values for the week for a specific counselor.

--- public/javascript/calendar_control.js
+++ public/javascript/calendar_control.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
+    populateCounselor();
     $("#counselor").on('change',showReasonPicker);
     $("#serviceSelector").on('change', showDatePicker);
 
     $("#datepicker").datepicker({
         beforeShowDay: $.datepicker.noWeekends,
+        minDate: new Date(),
         onSelect: function(dateText, inst) {
 
             var d = $(this).datepicker('getDate');
@@ -21,14 +23,21 @@ $(document).ready(function() {
 
             calendar.fullCalendar('changeView', 'agendaWeek');
             calendar.fullCalendar('gotoDate',d);
+            console.log(calendar.fullCalendar('clientEvents'));
             }
+
     });
-    document.getElementById("#submit").addEventListener("click", formSubmit);
+    document.getElementById("#submit").addEventListener("click", createAppointment);
 })
 
 var test1;
+var test2;
+var eventInfo;
 var eventObject;
+var eventJSON;
+var counselorJSON=[];
 
+var button = $('#form');
 var showReasonPicker= function ()
 {
     if($('#serviceSelector').css('display')=='none') {
@@ -37,6 +46,7 @@ var showReasonPicker= function ()
 }
 var showDatePicker= function()
 {
+    populateCalendar();
     if($('#datepicker').css('display')=='none') {
         $('#datepicker').show();
     }
@@ -57,10 +67,12 @@ var buildModal = function()
 
 var buildCalender=function(day)
 {
+
     var tempcalendar= $('#calendar').fullCalendar({
+
         height:700,
         minTime:"08:00:00",
-        maxTime:"23:00:00",
+        maxTime:"24:59:00",
         allDaySlot:false,
         columnFormat:{
             agenda:'ddd\n MMM Do'
@@ -69,9 +81,10 @@ var buildCalender=function(day)
         weekends: false,//hides weekend parameter
         firstDay:day,
         events:[{
-            title: '',
-            start: new Date(),
-        },],
+            title:'',
+            start:new Date(),
+
+        }],
 
         eventColor:'#FDD023',
         header: {
@@ -95,8 +108,12 @@ var buildCalender=function(day)
     });
     return tempcalendar;
 }
-
-function formSubmit()
+function createAppointment()
+{
+    formParse();
+    submitAppointment();
+}
+function formParse()
 {
     test1={
         "day":moment(eventObject.start).format('MMMM Do YYYY'),
@@ -107,3 +124,59 @@ function formSubmit()
         "student Id": $('#title').val(),
     };
 }
+
+function submitAppointment()
+{
+    //Push to counselor appoints.
+    //Delete from available appointments
+}
+
+ function getAvailableTimes()
+{
+    var eventArray=[];
+    $.getJSON("/api/advisor/availabletimes/"+$('#counselor option:selected').text(),function(data){
+            var i = 0;
+        $.each( data, function( key, val ) {
+            $.each(val, function()
+            {
+                console.log(val[i].available);
+                eventArray.push(val[i].available);
+                i++;
+            });
+            console.log(eventArray);
+        });
+    });
+    return eventArray;
+}
+
+function createEventArray(eventArray)
+{
+        var eventSource= [];
+    $.each(eventArray,function(availableTimes){
+       eventSource.push({
+           "title":"",
+           "start":""
+       });
+    });
+}
+
+function populateCalendar()
+{
+    var availableTimes = getAvailableTimes();
+
+    createEventArray(availableTimes);
+}
+
+function populateCounselor(){
+    $.getJSON("api/advisornames",function(data){
+        test2 = $('#counselor');
+        $.each(data, function(key,val){
+           test2.append($("<option></option>")
+                                  .attr("value",val).text(val));
+
+        })
+
+    });
+}
+
+
